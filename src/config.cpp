9e9602e38b8a1a0dd06351dd84660654d3dd4645
Sergey <mozgoff@gmail.com>
2019-11-03 01:02:24 +0100

    improved config constructor

--- src/config.cpp
+++ src/config.cpp
@@ -3,14 +3,8 @@
 
 Config::Config(const char name[8])
 {
-  axis_name[0] = name[0];
-  axis_name[1] = name[1];
-  axis_name[2] = name[2];
-  axis_name[3] = name[3];
-  axis_name[4] = name[4];
-  axis_name[5] = name[5];
-  axis_name[6] = name[6];
-  axis_name[7] = name[7];
+  for (char i = 0; i < 8; i++)
+    axis_name[i] = name[i];
 }
 
 // Create a global instance of the Config singleton
